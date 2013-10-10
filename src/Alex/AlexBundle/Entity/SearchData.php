f32c35cca62b0ee863efdb0aeae9c48d441e4176
alex <xalexcos@gmail.com>
2013-10-10 17:54:23 +0300

    second commit

---
+++ src/Alex/AlexBundle/Entity/SearchData.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alex
+ * Date: 10/10/13
+ * Time: 5:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Alex\AlexBundle\Entity;
+
+/**
+ * Class SearchData
+ * @package Alex\AlexBundle\Entity
+ */
+class SearchData {
+
+    /**
+     * @var string $date
+     */
+    public $date;
+    /**
+     * @var string $departAirport
+     */
+    public $departAirport;
+    /**
+     * @var string $arriveAirport
+     */
+    public $arriveAirport;
+
+}
